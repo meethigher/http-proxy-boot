e4f57ab47b77c9483577c5e3cb3b58a6268253a0
meethigher <meethigher@qq.com>
2024-06-29 18:05:29 +0800

    feat: 处理跨域时的options请求

--- src/main/java/top/meethigher/proxy/config/ProxyServletConfiguration.java
+++ src/main/java/top/meethigher/proxy/config/ProxyServletConfiguration.java
@@ -3,15 +3,23 @@ package top.meethigher.proxy.config;
 
 import org.mitre.dsmiley.httpproxy.ProxyServlet;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import top.meethigher.proxy.utils.CorsProxyServlet;
 
 import javax.annotation.Resource;
+import javax.servlet.Filter;
 import javax.servlet.Servlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.springframework.http.HttpHeaders.*;
 
 /**
  * SpringBoot使用ProxyServlet实现HTTP反向代理-阿里云开发者社区: https://developer.aliyun.com/article/1373393
@@ -46,6 +54,29 @@ public class ProxyServletConfiguration {
         params.put("log", String.valueOf(proxyProperties.getLog().isEnable()));
         registrationBean.setInitParameters(params);
         registrationBean.setName(proxyProperties.getName());
+        return registrationBean;
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = {"proxy.corsControl.enable", "proxy.corsControl.allowCORS"}, havingValue = "true")
+    public FilterRegistrationBean<Filter> optionsFilterRegistration() {
+        FilterRegistrationBean<Filter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter((servletRequest, servletResponse, filterChain) -> {
+            HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+            HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+            //预检请求无脑放行
+            if ("options".equalsIgnoreCase(httpServletRequest.getMethod())) {
+                httpServletResponse.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, httpServletRequest.getHeader("origin"));
+                httpServletResponse.setHeader(ACCESS_CONTROL_ALLOW_METHODS, "*");
+                httpServletResponse.setHeader(ACCESS_CONTROL_ALLOW_HEADERS, "*");
+                httpServletResponse.setHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+                httpServletResponse.setHeader(ACCESS_CONTROL_EXPOSE_HEADERS, "*");
+                httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+                return;
+            }
+            filterChain.doFilter(servletRequest, servletResponse);
+        });
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
         return registrationBean;
     }
 
