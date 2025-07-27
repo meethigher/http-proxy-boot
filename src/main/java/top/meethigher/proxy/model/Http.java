b03c77255c2b8507e26d0a5920310d5648fa57a8
meethigher <meethigher@qq.com>
2025-07-27 20:11:34 +0800

    feat: 调整默认配置

--- src/main/java/top/meethigher/proxy/model/Http.java
+++ src/main/java/top/meethigher/proxy/model/Http.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public class Http {
 
-    public Boolean enable=false;
-    public Integer port=8080;
-    public Boolean preheatDns=false;
-    public Integer maxThreads=1;
-    public Integer http1MaxSize=6000;
-    public Integer http2MaxSize=2000;
+    public Boolean enable = false;
+    public Integer port = 8080;
+    public Boolean preheatDns = false;
+    public Integer maxThreads = 1;
+    public Integer http1MaxSize = 6000;
+    public Integer http2MaxSize = 2000;
     public List<Router> routers = new ArrayList<Router>();
 
 }
--- src/main/java/top/meethigher/proxy/model/Router.java
+++ src/main/java/top/meethigher/proxy/model/Router.java
@@ -2,17 +2,17 @@ package top.meethigher.proxy.model;
 
 public class Router {
 
-    public String name;
-    public String sourceUrl;
-    public String targetUrl;
-    public Boolean forwardIp;
-    public Boolean preserveCookies;
-    public Boolean preserveHost;
-    public Boolean followRedirects;
-    public Boolean httpKeepAlive;
-    public Boolean logEnable;
-    public String logFormat;
-    public Boolean corsControl;
-    public Boolean corsAllow;
+    public String name = "route-default";
+    public String sourceUrl = "/*";
+    public String targetUrl = "http://127.0.0.1:80";
+    public Boolean forwardIp = false;
+    public Boolean preserveCookies = true;
+    public Boolean preserveHost = false;
+    public Boolean followRedirects = true;
+    public Boolean httpKeepAlive = true;
+    public Boolean logEnable = true;
+    public String logFormat = "{name} -- {serverHttpVersion} -- {clientHttpVersion} -- {method} -- {userAgent} -- {serverRemoteAddr} -- {serverLocalAddr} -- {clientLocalAddr} -- {clientRemoteAddr} -- {sourceUri} -- {proxyUrl} -- {statusCode} -- consumed {consumedMills} ms";
+    public Boolean corsControl = false;
+    public Boolean corsAllow = true;
 
 }
